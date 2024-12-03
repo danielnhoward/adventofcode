f03e71cf6a040d3460497f3faf7d1d5e2e63dd0d
danielnhoward <danielnhoward93@gmail.com>
2024-12-03 11:15:46 +0000

    Improve run logic

--- haskell/Utils/Run.hs
+++ haskell/Utils/Run.hs
@@ -8,23 +8,21 @@ type PartF a = Show a => String -> a
 type Year = Int
 type Day = Int
 
+runSingle :: Show a => String -> PartF a -> String -> IO ()
+runSingle text f path = readFile path >>= putStrLn . getText
+  where getText input = "  " ++ text ++ ": " ++ show (f input)
+
 runPart :: Show a => Part -> PartF a -> Year -> Day -> IO ()
 runPart part f year day = do
   runTests f (path "test") (path ("test" ++ show part))
-  input <- readFile (path "input")
-  putStrLn $ "  Input: " ++ show (f input)
+  runSingle "Main Input" f (path "input")
   where
     path s = "Year" ++ show year ++ "/input/" ++ show day ++ "/" ++ s ++ ".txt"
 
 runTests :: Show a => PartF a -> String -> String -> IO ()
-runTests f testPath testPartPath = doesFileExist testPath >>= (\exists ->
-  if exists then do
-    input <- readFile testPath
-    putStrLn $ "  Test Input: " ++ show (f input)
-  else do
-    input <- readFile testPartPath
-    putStrLn $ "  Test Input: " ++ show (f input)
-  )
+runTests f testPath testPartPath
+  = doesFileExist testPath >>= runSingle "Test Input" f . path
+  where path testExists = if testExists then testPath else testPartPath
 
 runA :: Show a => PartF a -> Year -> Day -> IO ()
 runA = runPart A
