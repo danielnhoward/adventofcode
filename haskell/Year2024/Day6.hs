c9672fb8f565fb85d9131a7300d90f441cbdce42
danielnhoward <danielnhoward93@gmail.com>
2024-12-06 15:34:09 +0000

    Optimise

--- haskell/Year2024/Day6.hs
+++ haskell/Year2024/Day6.hs
@@ -4,11 +4,14 @@ import Utils.List (getIndex)
 import Utils.Run (run)
 
 import Data.List (nub)
-import Data.Set (Set, empty, member, insert)
+import Data.Set (Set, empty, member, insert, size, difference, singleton, elems)
 import Data.Array (Array, array, assocs, (!), bounds)
+import Data.Maybe (fromJust)
 
 data Cell = Empty | Obstruction | Guard deriving (Eq, Show)
-type Grid = Array (Int, Int) Cell
+type Coordinate = (Int, Int)
+type Grid = Array Coordinate Cell
+type State = (Coordinate, Char)
 
 withIndex :: [a] -> [(Int, a)]
 withIndex = zip [0..]
@@ -27,10 +30,10 @@ procCell '>' = Guard
 procCell 'v' = Guard
 procCell '<' = Guard
 
-getGuardCoordinates :: Grid -> (Int, Int)
+getGuardCoordinates :: Grid -> Coordinate
 getGuardCoordinates grid = head [i | (i, e) <- assocs grid, e == Guard]
 
-direction :: Char -> (Int, Int)
+direction :: Char -> Coordinate
 direction '^' = (0, -1)
 direction '>' = (1, 0)
 direction 'v' = (0, 1)
@@ -43,48 +46,59 @@ turnRight 'v' = '<'
 turnRight '<' = '^'
 
 partA :: String -> Int
-partA input = length (nub (moves grid guardPos guard))
+partA input = size (moves grid (guardPos, guard))
   where
     grid = procInput input
     guardPos@(x, y) = getGuardCoordinates grid
     index = x + y * (2 + fst (snd (bounds grid)))
     guard = input !! index
 
-moves :: Grid -> (Int, Int) -> Char -> [(Int, Int)]
-moves grid pos@(x, y) guard
-  | x < 0 || y < 0 || x > maxX || y > maxY = []
-  | x' < 0 || y' < 0 || x' > maxX || y' > maxY = [(x, y)]
-  | grid ! newPos == Obstruction = moves grid pos (turnRight guard)
-  | otherwise = pos : moves grid newPos guard
+walk :: Grid -> State -> Maybe State
+walk = walkNewObstructions []
+
+walkNewObstructions :: [Coordinate] -> Grid -> State -> Maybe State
+walkNewObstructions newObstructions grid (pos@(x, y), guard)
+  | x' < 0 || y' < 0 || x' > maxX || y' > maxY = Nothing
+  | grid ! newPos == Obstruction || newPos `elem` newObstructions
+    = Just (pos, turnRight guard)
+  | otherwise = Just (newPos, guard)
   where
     (maxX, maxY) = snd (bounds grid)
     (dirX, dirY) = direction guard
     newPos@(x', y') = (x + dirX, y + dirY)
 
+moves :: Grid -> State -> Set Coordinate
+moves grid firstState = go firstState empty
+  where
+    go :: State -> Set Coordinate -> Set Coordinate
+    go state@(pos@(x, y), guard) history = case walk grid state of
+      Just newState -> go newState history'
+      Nothing -> history'
+      where history' = pos `insert` history
+
 partB :: String -> Int
-partB input = length (filter (loops grid guardPos guard) coordinatesList)
+partB input = length (filter (loops grid (guardPos, guard)) (elems coordsList))
   where
     grid = procInput input
     guardPos@(x, y) = getGuardCoordinates grid
     index = x + y * (2 + fst (snd (bounds grid)))
     guard = input !! index
-    (_ : coordinatesList) = nub (moves grid guardPos guard)
+    coordsList = difference (moves grid (guardPos, guard)) (singleton guardPos)
 
-loops :: Grid -> (Int, Int) -> Char -> (Int, Int) -> Bool
-loops grid startPos startGuard newObstruction = go startPos startGuard empty
+loops :: Grid -> State -> Coordinate -> Bool
+loops grid startState newObstruction = case walk' grid startState of
+  Just nextState -> go startState nextState 1 1
+  Nothing -> False
   where
-    (maxX, maxY) = snd (bounds grid)
-    go :: (Int, Int) -> Char -> Set (Char, Int, Int) -> Bool
-    go pos@(x, y) guard history
-      | x < 0 || y < 0 || x > maxX || y > maxY = False
-      | x' < 0 || y' < 0 || x' > maxX || y' > maxY = False
-      | (guard, x, y) `member` history = True
-      | grid ! newPos == Obstruction || newPos == newObstruction
-        = go pos (turnRight guard) ((guard, x, y) `insert` history)
-      | otherwise = go newPos guard history
-      where
-        (dirX, dirY) = direction guard
-        newPos@(x', y') = (x + dirX, y + dirY)
+    walk' = walkNewObstructions [newObstruction]
+    go :: State -> State -> Int -> Int -> Bool
+    go slowState fastState power length
+      | slowState == fastState = True
+      | otherwise = case walk' grid fastState of
+        Just newState -> if power == length
+          then go fastState newState (power * 2) 1
+          else go slowState newState power (length + 1)
+        Nothing -> False
 
 main :: IO ()
 main = run partA partB 2024 6
